ccf96f072b9002fd05df9705efa35a98efeeede9
NGUYENNHATANH16072005 <nguyennhatanh16072005@gmail.com>
2024-08-25 18:23:13 +0700

    Add files via upload

---
+++ vd2.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Aug 25 13:18:53 2024
+
+@author: nhat anh
+"""
+a = 10
+b = 3
+print ("a+b=", a+b)
+print ("a-b=", a-b)
+print ("a*b=", a*b)
+print ("a/b=", a/b)
+print ("a%b=", a%b)
+print ("a//b=", a//b)
+x=3
+print("a^x=",a**x)
+
