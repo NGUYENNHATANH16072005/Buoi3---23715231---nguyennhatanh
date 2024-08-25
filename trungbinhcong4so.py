72f347289537a0c155d5ed9ff2035d98f0154443
NGUYENNHATANH16072005 <nguyennhatanh16072005@gmail.com>
2024-08-25 16:22:20 +0700

    Add files via upload

---
+++ trungbinhcong4so.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Aug 25 13:22:30 2024
+
+@author: nhat anh
+"""
+a = int(input("So nguyen a:"))
+b = int(input("So nguyen b:"))
+c = int(input("So nguyen c:"))
+d = int(input("So nguyen d:"))
+print ("Trung binh cong cua 4 so tren la", ((a+b+c+d)/4))
