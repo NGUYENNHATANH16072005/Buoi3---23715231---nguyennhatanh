5dd8b360a89ce87505355562e88dce477b87048d
NGUYENNHATANH16072005 <nguyennhatanh16072005@gmail.com>
2024-08-25 16:21:59 +0700

    Add files via upload

---
+++ tuoi.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Aug 25 13:48:51 2024
+
+@author: nhat anh
+"""
+a = int(input("Nam sinh cua ban la:"))
+b = int(input("Nam hien tai la:"))
+x = b-a
+print ("So tuoi cua ban la", x)
