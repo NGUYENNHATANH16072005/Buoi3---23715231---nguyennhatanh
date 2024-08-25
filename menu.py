c1e01ee40d0bd92cb2a6a7936bfc18f2c3adc70f
NGUYENNHATANH16072005 <nguyennhatanh16072005@gmail.com>
2024-08-25 16:23:17 +0700

    Add files via upload

---
+++ menu.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Aug 25 14:02:49 2024
+
+@author: nhat anh
+"""
+print("============MENU============")
+print("1.Hu tieu")
+print("2.Chao long")
+print("3.Banh canh")
+print("4.Bun rieu")
+print("5.Pho bo")
+print("==========================")
+
+a=float(input("Moi nhap lua chon:"))
+if a==1:
+    print("1.Hu tieu")
+elif a==2:
+    print("2.Chao long")
+elif a==3:
+    print("3.Banh canh")
+elif a==4:
+    print("4.Bun rieu")
+elif a==5:
+    print("5.Pho bo")
+else:
+    print("Lua chon cua quy khach khong hop le")
+
+print("==========================")
+
+
+        
+        
