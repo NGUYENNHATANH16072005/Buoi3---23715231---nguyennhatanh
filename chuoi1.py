57bde2dd2019b48d2f88cc1f8337fe8062167443
NGUYENNHATANH16072005 <nguyennhatanh16072005@gmail.com>
2024-08-25 16:24:53 +0700

    Add files via upload

---
+++ chuoi1.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Aug 25 15:07:16 2024
+
+@author: nhat anh
+"""
+print("In ra lan 1: ")
+
+var1 = 'Đại học Quốc Gia'
+print("Thành var1[0:16] ", var1[0:16])
+var2 = 'Khu phố 6'
+print("Thành var2[0:9]", var2[0:9])
+var3 = 'P.Linh Trung'
+print("Thành var3[0:12]",var3[0:12])
+var4 = 'Q.Thủ Đức'
+print("Thành var4[0:9]",var4[0:9])
+var5 = 'Tp.HCM'
+print("Thành var5[0:6]",var5[0:6])
+
+print(" In ra lan 2: ")
+    
+var1 = 'Đại học Quốc Gia'
+print("Thành var1[0:16] ", var1[0:16])
+var2 = 'Khu phố 6'
+print("Thành var2[0:9]", var2[0:9])
+var3 = 'P.Linh Trung'
+print("Thành var3[2:12]",var3[2:12])
+var4 = 'Q.Thủ Đức'
+print("Thành var4[2:9]",var4[2:9])
+var5 = 'Tp.HCM'
+print("Thành var5[3:6]",var5[3:6])
