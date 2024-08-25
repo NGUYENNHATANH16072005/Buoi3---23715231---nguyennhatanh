45f6e665431057532054cdb284fb9d3a72e9827d
NGUYENNHATANH16072005 <nguyennhatanh16072005@gmail.com>
2024-08-25 16:22:40 +0700

    Add files via upload

---
+++ tonghieutichthuong2so.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Aug 25 13:28:37 2024
+
+@author: nhat anh
+"""
+
+a = int(input("So nguyen a:"))
+b = int(input("So nguyen b:"))
+print ("Tong 2 so tren la", (a+b))
+print ("Hieu 2 so tren la", (a-b))
+print ("Tich 2 so tren la", (a*b))
+x=a/b
+print ("Thuong 2 so tren la:", round(x,3))
+print ("Chia lay du 2 so tren la", (a%b))
+print ("Chia lay nguyen 2 so tren la", (a//b))
