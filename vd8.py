56efd99054bb2c5adc01363a9a8845dd763f2b62
NGUYENNHATANH16072005 <nguyennhatanh16072005@gmail.com>
2024-08-25 18:37:59 +0700

    Add files via upload

---
+++ vd8.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Aug 25 18:33:56 2024
+
+@author: nhat anh
+"""
+name = None
+print(name)
+name = 'Van B'
+print(name)
+value = 5
+if value is None:
+    print("Value co gia tri Nono")
+else:
+        print("Value=",value)
