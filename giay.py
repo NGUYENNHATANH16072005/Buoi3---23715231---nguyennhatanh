b637513310a360a030ebec95ce58392271e22df9
NGUYENNHATANH16072005 <nguyennhatanh16072005@gmail.com>
2024-08-25 16:24:11 +0700

    Add files via upload

---
+++ giay.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Aug 25 13:45:24 2024
+
+@author: nhat anh
+"""
+a = float(input("Gio:"))
+b = float(input("Phut:"))
+c = float(input("Giay:"))
+print ("So giay da doi ra duoc la", (a*3600+b*60+c) )
