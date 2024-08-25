229c9e474b9bb8d0f798e27e31312b84b6491a5d
NGUYENNHATANH16072005 <nguyennhatanh16072005@gmail.com>
2024-08-25 18:32:43 +0700

    Add files via upload

---
+++ vd3.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Aug 25 17:38:22 2024
+
+@author: nhat anh
+"""
+import math
+import random
+
+prinṭ(math.sqrt(9))
+print(math.fabs(-9))
+print(math.modf(5))
+print(math.fmod(5, 7))
+
+print(random.random())
+print(random.randrange(3))
+print(random.randrange(1, 4, 3))
+    
+
