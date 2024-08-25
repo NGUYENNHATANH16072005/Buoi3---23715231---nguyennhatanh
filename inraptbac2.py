079671ac8098ff261e487ccf54d51533cac16fbf
NGUYENNHATANH16072005 <nguyennhatanh16072005@gmail.com>
2024-08-25 16:23:47 +0700

    Add files via upload

---
+++ inraptbac2.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Aug 25 13:56:41 2024
+
+@author: nhat anh
+"""
+a = int(input("So a:"))
+b = int(input("So b:"))
+c = int(input("So c:"))
+if a!=0:
+   print(f"Phương trình bậc hai có dạng: {a}x**2 + {b}x + {c} = 0")
+else:
+   print("Cac so khong hop le de viet phuong trinh") 
+    
