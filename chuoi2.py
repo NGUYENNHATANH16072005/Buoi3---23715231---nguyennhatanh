bffd7c9a2ec1331a7f3c928da352ebd182ed3d40
NGUYENNHATANH16072005 <nguyennhatanh16072005@gmail.com>
2024-08-25 16:24:40 +0700

    Add files via upload

---
+++ chuoi2.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Aug 25 16:14:24 2024
+
+@author: nhat anh
+"""
+var1= "i'm a cat"
+var2= "I'm A Cat"
+
+print(var1.title())
+
+print(var1.upper())
+
+print(var2.swapcase())
+
+print(var1.capitalize())
